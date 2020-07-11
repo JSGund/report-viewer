2eeded8a9495f9bd91902ffc60c41b90a4c95d6f
Jeetendra Gund <jgund@cytrack.com>
2020-07-11 18:06:02 +0530

    report viewer added

---
+++ WPF/wpf-report-viewer/wpf-report-viewer/TelerikReportViewer.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace wpf_report_viewer
+{
+    using System;
+    using System.Windows;
+    using Telerik.Windows.Controls;
+
+    public partial class TelerikReportViewer : Window
+    {
+        public TelerikReportViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
